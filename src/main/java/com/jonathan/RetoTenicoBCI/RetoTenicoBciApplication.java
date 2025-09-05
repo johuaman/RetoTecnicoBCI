7105f6728f16bc42bf2129a90b13ec6c2ebc348e
johuaman <101164115+johuaman@users.noreply.github.com>
2025-09-04 23:33:28 -0500

    Inicializate project RetoTecnicoBCI

---
+++ src/main/java/com/jonathan/RetoTenicoBCI/RetoTenicoBciApplication.java
@@ -0,0 +1,13 @@
+package com.jonathan.RetoTenicoBCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoTenicoBciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoTenicoBciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathan/RetoTenicoBCI/RetoTenicoBciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.RetoTenicoBCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoTenicoBciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
